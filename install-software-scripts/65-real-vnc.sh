f267883cc9ae9fa8644920bd1e57289bfa431ff1
Samuel Casanova <samuel.casanova@gmail.com>
2024-08-28 07:19:45 +0200

    stow real vnc config

--- install-software-scripts/65-real-vnc.sh
+++ install-software-scripts/65-real-vnc.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo -e '\nInstalling RealVNC\n'
+echo -e '\nInstalling Real VNC Viewer\n'
 
 read -p 'Press Enter to open the download page. You will need to download the Linux RPM x64 package and copy the file location. You will be prompted for this download location in the next step.'
 xdg-open https://www.realvnc.com/en/connect/download/viewer/linux/
 read -p 'Paste the local file location and press ENTER: ' REALVNC_INSTALLER_PATH
-
 sudo dnf install ${REALVNC_INSTALLER_PATH}
+
+mv ~/.vnc /tmp
+stow  -v -d ~/git/linux-setup-private/dotfiles/ -t ~ realvnc
