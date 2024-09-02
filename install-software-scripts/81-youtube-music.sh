33b64d5f8e67594ec4cc0160f4e38a21afa1b7bb
Samuel Casanova <samuel.casanova@gmail.com>
2024-09-02 11:06:38 +0200

    add youtube-music

---
+++ install-software-scripts/81-youtube-music.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -e '\nInstalling youtube music client with no ads\n'
+
+read -p 'Press ENTER to open the downloads page and download the latest RPM available. You should copy the local path to the downloaded file.'
+xdg-open https://github.com/th-ch/youtube-music/releases
+read -p 'Paste the current local path to the RPM file: ' RPM_LOCAL_PATH
+wget -qO- "https://raw.githubusercontent.com/nvm-sh/nvm/v${RPM_LOCAL_PATH}/install.sh" | bash
+
+sudo dnf install -y "${RPM_LOCAL_PATH}"
