0bba21f0246bdd65663010af5783e7e5002e528f
Samuel Casanova <samuel.casanova@gmail.com>
2024-09-02 11:26:39 +0200

    add kde konsole settings

---
+++ dotfiles/kde/.local/share/konsole/Samuel.profile
@@ -0,0 +1,6 @@
+[Appearance]
+Font=Monospace,8,-1,5,400,0,0,0,0,0,0,0,0,0,0,1
+
+[General]
+Name=Samuel
+Parent=FALLBACK/
--- install-software-scripts/12-kde.sh
+++ install-software-scripts/12-kde.sh
@@ -2,16 +2,17 @@
 
 echo -e '\nConfiguring KDE\n'
 
-mkdir -p /tmp/.config
+mkdir -p /tmp/kde/.config
 
-mv ~/.config/discoverrc /tmp/.config
-mv ~/.config/dolphinrc /tmp/.config
-mv ~/.config/kdeglobals /tmp/.config
-mv ~/.config/kglobalshortcutsrc /tmp/.config
-mv ~/.config/konsolerc /tmp/.config
-mv ~/.config/konsolesshconfig /tmp/.config
-mv ~/.config/plasmarc /tmp/.config
-mv ~/.config/xsettingsd /tmp/.config
-mv ~/.config/ksmserverrc /tmp/.config
+mv ~/.config/discoverrc /tmp/kde/.config
+mv ~/.config/dolphinrc /tmp/kde/.config
+mv ~/.config/kdeglobals /tmp/kde/.config
+mv ~/.config/kglobalshortcutsrc /tmp/kde/.config
+mv ~/.config/konsolerc /tmp/kde/.config
+mv ~/.config/konsolesshconfig /tmp/kde/.config
+mv ~/.config/plasmarc /tmp/kde/.config
+mv ~/.config/xsettingsd /tmp/kde/.config
+mv ~/.config/ksmserverrc /tmp/kde/.config
+mv ~/.local/share/konsole /tmp/kde
 
 stow  -v -d ~/git/linux-setup/dotfiles/ -t ~ kde
