ab63f418811091ddbedeb14c2a3c561345dde222
samuel.casanova <samuel.casanova@immfly.com>
2024-09-03 20:31:52 +0200

    chmod ssh keys

--- install-software-scripts/15-ssh.sh
+++ install-software-scripts/15-ssh.sh
@@ -4,4 +4,5 @@ echo -e '\nConfiguring SSH\n'
 
 if [ -d ~/.ssh ]; then mv ~/.ssh /tmp; fi
 stow -v -d ~/git/linux-setup-private/dotfiles/ -t ~ ssh
-                
+chmod 600 ~/git/linux-setup-private/dotfiles/ssh/.ssh/linux*
+chmod 600 ~/git/linux-setup-private/dotfiles/ssh/.ssh/id*
