05f2d8089c9dead44e4a8be2ad4679903e714bc2
samuel.casanova <samuel.casanova@immfly.com>
2024-09-03 20:05:01 +0200

    change install instructions for docker Ubuntu

--- install-software-scripts/30-docker.sh
+++ install-software-scripts/30-docker.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
 echo -e '\nInstalling docker\n'
+# https://docs.docker.com/engine/install/ubuntu/
 
-sudo apt install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get -y install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 sudo systemctl start docker
 
 echo -e '\nConfiguring docker for non-root user samuel (2/2)\n'
 
 # https://docs.docker.com/engine/install/linux-postinstall/
-sudo groupadd docker
+if [ $(getent group docker) ]; then sudo groupadd docker; fi
 sudo usermod -aG docker $USER
 newgrp docker
 
