14291e6f1b1aada8129e649ed5b0767370d75b80
samuel.casanova <samuel.casanova@immfly.com>
2024-09-05 07:17:14 +0200

    fix nvm command not found during installation

--- install-software-scripts/35-nvm.sh
+++ install-software-scripts/35-nvm.sh
@@ -3,10 +3,12 @@
 echo -e '\nInstalling NVM and Node versions\n'
 
 read -p 'Press ENTER to open the install instructions on your default browser. You should copy the current version (i.e. 0.40.0) and go back to this script.'
-xdg-open https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating
+xdg-open https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating &> /dev/null
 read -p 'Paste the current NVM version (i.e. 0.40.0): ' NVM_VERSION
+echo -e "Downloading from https://raw.githubusercontent.com/nvm-sh/nvm/v${NVM_VERSION}/install.sh"
 wget -qO- "https://raw.githubusercontent.com/nvm-sh/nvm/v${NVM_VERSION}/install.sh" | bash || if [ ${?} -gt 0 ]; then exit 1; fi
 source ~/.bashrc
+source ~/.nvm/nvm.sh
 
 nvm install v22 || if [ ${?} -gt 0 ]; then exit 1; fi
 nvm install v20 || if [ ${?} -gt 0 ]; then exit 1; fi
@@ -14,3 +16,4 @@ nvm install v18 || if [ ${?} -gt 0 ]; then exit 1; fi
 nvm install v16 || if [ ${?} -gt 0 ]; then exit 1; fi
 nvm install v10 || if [ ${?} -gt 0 ]; then exit 1; fi
 nvm install v8.4.0 || if [ ${?} -gt 0 ]; then exit 1; fi
+
