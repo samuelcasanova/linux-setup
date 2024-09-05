bf830d32d660bf52a6a6f81286ec17c4ec8c66bb
samuel.casanova <samuel.casanova@immfly.com>
2024-09-05 09:18:05 +0200

    change real vnc instructions to download the deb file instead of rpm

--- install-software-scripts/65-real-vnc.sh
+++ install-software-scripts/65-real-vnc.sh
@@ -2,7 +2,7 @@
 
 echo -e '\nInstalling Real VNC Viewer\n'
 
-read -p 'Press Enter to open the download page. You will need to download the Linux RPM x64 package and copy the file location. You will be prompted for this download location in the next step.'
+read -p 'Press Enter to open the download page. You will need to download the Linux DEB x64 package and copy the file location. You will be prompted for this download location in the next step.'
 xdg-open https://www.realvnc.com/en/connect/download/viewer/linux/
 read -p 'Paste the local file location and press ENTER: ' REALVNC_INSTALLER_PATH
 sudo apt install ${REALVNC_INSTALLER_PATH} || if [ ${?} -gt 0 ]; then exit 1; fi
