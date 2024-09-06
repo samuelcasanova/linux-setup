bf89eeb9c187f490ba5dbea341d1c8048aead6d7
samuel.casanova <samuel.casanova@immfly.com>
2024-09-06 07:59:14 +0200

    fix youtube-music and safeeyes installers for Ubuntu

--- install-software-scripts/80-safeeyes.sh
+++ install-software-scripts/80-safeeyes.sh
@@ -2,12 +2,9 @@
 
 echo -e '\nInstalling safeeyes\n'
 
-echo 'ÇCheck instructions for debian'
-exit 1
+sudo add-apt-repository ppa:safeeyes-team/safeeyes || if [ ${?} -gt 0 ]; then exit 1; fi
+sudo apt update || if [ ${?} -gt 0 ]; then exit 1; fi
+sudo apt-get -y install safeeyes || if [ ${?} -gt 0 ]; then exit 1; fi
 
-sudo apt install libappindicator-gtk3 python3-psutil || if [ ${?} -gt 0 ]; then exit 1; fi
-sudo apt install python3-pip || if [ ${?} -gt 0 ]; then exit 1; fi
-sudo pip3 install safeeyes || if [ ${?} -gt 0 ]; then exit 1; fi
-sudo gtk-update-icon-cache /usr/share/icons/hicolor || if [ ${?} -gt 0 ]; then exit 1; fi
 if [ -d ~/.config/safeeyes ]; then mv ~/.config/safeeyes /tmp; fi
 stow -v -d ~/git/linux-setup/dotfiles/ -t ~ safeeyes || if [ ${?} -gt 0 ]; then exit 1; fi
--- install-software-scripts/81-youtube-music.sh
+++ install-software-scripts/81-youtube-music.sh
@@ -2,9 +2,7 @@
 
 echo -e '\nInstalling youtube music client with no ads\n'
 
-read -p 'Press ENTER to open the downloads page and download the latest RPM available. You should copy the local path to the downloaded file.'
+read -p 'Press ENTER to open the downloads page and download the latest DEB available. You should copy the local path to the downloaded file.'
 xdg-open https://github.com/th-ch/youtube-music/releases
-read -p 'Paste the current local path to the RPM file: ' RPM_LOCAL_PATH
-wget -qO- "https://raw.githubusercontent.com/nvm-sh/nvm/v${RPM_LOCAL_PATH}/install.sh" | bash || if [ ${?} -gt 0 ]; then exit 1; fi
-
-sudo apt install "${RPM_LOCAL_PATH}" || if [ ${?} -gt 0 ]; then exit 1; fi
+read -p 'Paste the current local path to the DEB file: ' DEB_LOCAL_PATH
+sudo apt-get -y install "${DEB_LOCAL_PATH}" || if [ ${?} -gt 0 ]; then exit 1; fi
