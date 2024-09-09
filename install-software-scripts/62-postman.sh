3a065bab4ed07f6f3e2453e3b120b8cff7d1842d
samuel.casanova <samuel.casanova@immfly.com>
2024-09-09 09:14:32 +0200

    install postman

---
+++ install-software-scripts/62-postman.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -e '\nInstalling Postman\n'
+
+wget -O /tmp/postman.tar.gz https://dl.pstmn.io/download/latest/linux64 || if [ ${?} -gt 0 ]; then exit 1; fi
+mkdir -p /tmp/postman
+tar -C /tmp/postman -zxf /tmp/postman.tar.gz
+sudo mv /tmp/postman/Postman /opt
+sudo cp ~/git/linux-setup/install-software-scripts/support-files/postman.desktop /usr/share/applications/postman.desktop
