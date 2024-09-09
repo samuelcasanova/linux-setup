01991597821bc044bb5d39403ccb353a930eea61
samuel.casanova <samuel.casanova@immfly.com>
2024-09-09 09:59:16 +0200

    make safeeyes start at login

--- install-software-scripts/80-safeeyes.sh
+++ install-software-scripts/80-safeeyes.sh
@@ -7,4 +7,5 @@ sudo apt update || if [ ${?} -gt 0 ]; then exit 1; fi
 sudo apt-get -y install safeeyes || if [ ${?} -gt 0 ]; then exit 1; fi
 
 if [ -d ~/.config/safeeyes ]; then mv ~/.config/safeeyes /tmp; fi
+if [ -f ~/.config/autostart/io.github.slgobinath.SafeEyes.desktop ]; then mv ~/.config/autostart/io.github.slgobinath.SafeEyes.desktop /tmp; fi
 stow -v -d ~/git/linux-setup/dotfiles/ -t ~ safeeyes || if [ ${?} -gt 0 ]; then exit 1; fi
