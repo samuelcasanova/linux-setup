ab3e3246160c3fec0b8e343ba308b7ac99b54c6a
samuel.casanova <samuel.casanova@immfly.com>
2024-09-10 07:50:48 +0200

    disable single click, increase screen locking timeout, and add tiling and task switcher options

--- install-software-scripts/12-kde.sh
+++ install-software-scripts/12-kde.sh
@@ -13,6 +13,8 @@ if [ -f ~/.config/konsolesshconfig ]; then mv ~/.config/konsolesshconfig /tmp/kd
 if [ -f ~/.config/plasmarc ]; then mv ~/.config/plasmarc /tmp/kde/.config; fi
 if [ -d ~/.config/xsettingsd ]; then mv ~/.config/xsettingsd /tmp/kde/.config; fi
 if [ -f ~/.config/ksmserverrc ]; then mv ~/.config/ksmserverrc /tmp/kde/.config; fi
+if [ -f ~/.config/kwinrc ]; then mv ~/.config/kwinrc /tmp/kde/.config; fi
+if [ -f ~/.config/kscreenlockerrc ]; then mv ~/.config/kscreenlockerrc /tmp/kde/.config; fi
 if [ -d ~/.local/share/konsole ]; then mv ~/.local/share/konsole /tmp/kde; fi
 
 stow  -v -d ~/git/setups/linux-setup/dotfiles/ -t ~ kde || if [ ${?} -gt 0 ]; then exit 1; fi
