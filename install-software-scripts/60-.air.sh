63172b7c5e82d2be4ba8b30e2cec82736152ab9f
samuel.casanova <samuel.casanova@immfly.com>
2024-09-11 19:29:32 +0200

    fix .air .sh extension in air-local

--- install-software-scripts/60-.air.sh
+++ install-software-scripts/60-.air.sh
@@ -2,7 +2,7 @@
 
 echo -e '\nInstalling .air air-shopping dependencies\n'
 
-# Imagemagick v7.1 already installed in Fedora
+# Imagemagick v7.1 already installed in Ubuntu
 sudo apt-get -y install tini || if [ ${?} -gt 0 ]; then exit 1; fi
 sudo apt-get -y install apache2-utils || if [ ${?} -gt 0 ]; then exit 1; fi
 
@@ -13,7 +13,7 @@ pushd ~/git/iag
 
 git clone git@gitlab.com:iag-connect/tools/air-local.git || if [ ${?} -gt 0 ]; then exit 1; fi
 pushd air-local
-./air-local init || if [ ${?} -gt 0 ]; then exit 1; fi
+./air-local.sh init || if [ ${?} -gt 0 ]; then exit 1; fi
 echo -e "TAIL=0G-IFSC\nCAPTCHA_BYPASS=false" > ./.env || if [ ${?} -gt 0 ]; then exit 1; fi
 sed 's/0G-IFXX/0G-IFSC/g' ./projects/air-simulator/.env || if [ ${?} -gt 0 ]; then exit 1; fi
 echo "TAIL_NUMBER=0G-IFSC" > ./projects/air-rabbitmq-proxy/.env || if [ ${?} -gt 0 ]; then exit 1; fi
