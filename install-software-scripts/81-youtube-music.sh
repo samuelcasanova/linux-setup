f0867a29ae3fdaf49c1c9a56e887f5dbe5d3abc3
samuel.casanova <samuel.casanova@immfly.com>
2024-09-12 09:36:16 +0200

    fix youtube music shortcut

--- install-software-scripts/81-youtube-music.sh
+++ install-software-scripts/81-youtube-music.sh
@@ -6,3 +6,4 @@ read -p 'Press ENTER to open the downloads page and download the latest DEB avai
 xdg-open https://github.com/th-ch/youtube-music/releases
 read -p 'Paste the current local path to the DEB file: ' DEB_LOCAL_PATH
 sudo apt-get -y install "${DEB_LOCAL_PATH}" || if [ ${?} -gt 0 ]; then exit 1; fi
+sudo sed -i 's/\%U$/--no-sandbox \%U/g' /usr/share/applications/youtube-music.desktop || if [ ${?} -gt 0 ]; then exit 1; fi
