bb2d6e29877be65a2092f25b30eba05b76b1a73f
samuel.casanova <samuel.casanova@immfly.com>
2024-09-25 18:00:46 +0200

    add brave software test

--- install-software-scripts/16-chrome.sh
+++ install-software-scripts/16-chrome.sh
@@ -7,3 +7,8 @@ echo -e 'Installing Chrome\n'
 wget -O /tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i /tmp/chrome.deb
 
+#Testing Brave Browser
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt-get update
+sudo apt-get -y install brave-browser
