5e5b2374be6a6cbaefa1ac0492aaa32e04692e4f
samuel.casanova <samuel.casanova@immfly.com>
2024-10-17 16:41:46 +0200

    add gitlab registry token

--- install-software-scripts/10-bash.sh
+++ install-software-scripts/10-bash.sh
@@ -3,4 +3,7 @@
 echo -e '\nConfiguring bash shell\n'
 
 if [ -f ~/.bashrc ]; then mv ~/.bashrc /tmp; fi
+if [ -f ~/.bash_env_vars ]; then mv ~/.bash_env_vars /tmp; fi
+
 stow  -v -d ~/git/setups/linux-setup/dotfiles/ -t ~ bash || if [ ${?} -gt 0 ]; then exit 1; fi
+stow  -v -d ~/git/setups/linux-setup-private/dotfiles/ -t ~ bash || if [ ${?} -gt 0 ]; then exit 1; fi
