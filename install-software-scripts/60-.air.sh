fe266918220cdc7913038ed590c5ce45cd1634a2
samuel.casanova <samuel.casanova@immfly.com>
2025-02-07 08:20:57 +0100

    unify all .air config

--- install-software-scripts/60-.air.sh
+++ install-software-scripts/60-.air.sh
@@ -39,33 +39,47 @@ cp src/configs/local.example.config.ts src/configs/local.config.ts
 cp src/configs/env/local/local.template.env src/configs/env/local/local.env
 sed -i 's/0G-IFXX/0G-IFSC/g' src/configs/env/local/local.env || if [ ${?} -gt 0 ]; then exit 1; fi
 
+git clone git@gitlab.com:iag-connect/qa/iag-rack-tools.git || if [ ${?} -gt 0 ]; then exit 1; fi
+
 popd
 
 mkdir -p ~/git/iag/grd
 pushd ~/git/iag/grd
 
 git clone git@gitlab.com:iag-connect/groundside/grd-ota-data.git || if [ ${?} -gt 0 ]; then exit 1; fi
+cp ~/git/setups/linux-setup-private/dot-air/grd-ota-data.env ~/git/iag/grd/grd-ota-data/.env || if [ ${?} -gt 0 ]; then exit 1; fi
+
 cp grd-ota-data/.env-template grd-ota-data/.env
 git clone git@gitlab.com:iag-connect/groundside/grd-flight-data.git || if [ ${?} -gt 0 ]; then exit 1; fi
 git clone git@gitlab.com:iag-connect/groundside/grd-entitlements-v2.git || if [ ${?} -gt 0 ]; then exit 1; fi
+
 git clone git@gitlab.com:iag-connect/groundside/grd-internet-viasat.git || if [ ${?} -gt 0 ]; then exit 1; fi
+cp ~/git/iag/grd/grd-internet-viasat/config/_test.json-template ~/git/iag/grd/grd-internet-viasat/config/test.json || if [ ${?} -gt 0 ]; then exit 1; fi
+cp ~/git/iag/grd/grd-internet-viasat/config/_development.json-template ~/git/iag/grd/grd-internet-viasat/config/development.json || if [ ${?} -gt 0 ]; then exit 1; fi
 
 popd
 
 mkdir -p ~/git/iag/libs
 pushd ~/git/iag/libs
 
-git clone git@gitlab.com:iag-connect/groundside/node-config-lib.git || if [ ${?} -gt 0 ]; then exit 1; fi
-git clone git@gitlab.com:iag-connect/groundside/node-logging-lib.git || if [ ${?} -gt 0 ]; then exit 1; fi
-git clone git@gitlab.com:iag-connect/groundside/node-amqp-lib.git || if [ ${?} -gt 0 ]; then exit 1; fi
-git clone git@gitlab.com:iag-connect/groundside/node-shared-types-lib.git || if [ ${?} -gt 0 ]; then exit 1; fi
-git clone git@gitlab.com:iag-connect/groundside/node-dependency-injection-lib.git || if [ ${?} -gt 0 ]; then exit 1; fi
+git clone git@gitlab.com:iag-connect/node-libs/node-config-lib.git || if [ ${?} -gt 0 ]; then exit 1; fi
+git clone git@gitlab.com:iag-connect/node-libs/node-logging-lib.git || if [ ${?} -gt 0 ]; then exit 1; fi
+git clone git@gitlab.com:iag-connect/node-libs/node-shared-types-lib.git || if [ ${?} -gt 0 ]; then exit 1; fi
+git clone git@gitlab.com:iag-connect/node-libs/node-db-lib.git || if [ ${?} -gt 0 ]; then exit 1; fi
+git clone git@gitlab.com:iag-connect/node-libs/node-amqp-lib.git || if [ ${?} -gt 0 ]; then exit 1; fi
+git clone git@gitlab.com:iag-connect/node-libs/node-dependency-injection-lib.git || if [ ${?} -gt 0 ]; then exit 1; fi
+
+popd
+
+mkdir -p ~/git/iag/tools
+pushd ~/git/iag/tools
+
+git clone git@gitlab.com:iag-connect/tools/iag-process-helper.git || if [ ${?} -gt 0 ]; then exit 1; fi
+cp ~/git/setups/linux-setup-private/dot-air/iag-process-helper.env ~/git/iag/tools/iag-process-helper/.env || if [ ${?} -gt 0 ]; then exit 1; fi
 
 popd
 
 popd
-
-
 
 if [ -f ~/.npmrc ]; then mv ~/.npmrc /tmp/; fi
 
