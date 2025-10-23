bb16bf3a7dcbf424e86d83e97f553728aa24be41
samuel.casanova <samuel.casanova@immfly.com>
2025-10-23 11:22:16 +0200

    add immfly backend route service

--- install-software-scripts/60-.immfly-wifec.sh
+++ install-software-scripts/60-.immfly-wifec.sh
@@ -3,13 +3,22 @@
 echo -e '\nInstalling Immfly wIFEC dependencies and projects\n'
 
 mkdir -p ~/git/immfly
-pushd ~/git/immfly
-
+pushd ~/git/immfly/
 echo -e '\nInstalling local environment (backend services)\n'
 git clone git@git.immfly.com:immfly/local-environment.git || if [ ${?} -gt 0 ]; then exit 1; fi
 ~/git/setups/linux-setup-private/install-software-scripts/support-files/immfly-wifec/setup-wifec.sh || if [ ${?} -gt 0 ]; then exit 1; fi
+popd
 
+mkdir -p ~/git/immfly/frontend
+pushd ~/git/immfly/frontend
 git clone git@git.immfly.com:immfly/ife-app.git || if [ ${?} -gt 0 ]; then exit 1; fi
 cp ~/git/setups/linux-setup-private/immfly-wifec/ife-app/.npmrc ~/git/immfly/ife-app/.npmrc || if [ ${?} -gt 0 ]; then exit 1; fi
+git clone git@git.immfly.com:immfly/voe-magazine.git || if [ ${?} -gt 0 ]; then exit 1; fi
+popd
+
+mkdir -p ~/git/immfly/backend
+pushd ~/git/immfly/backen
+git clone git@git.immfly.com:immfly/microservices/routes/service.git route-service || if [ ${?} -gt 0 ]; then exit 1; fi
+popd
 
 sudo apt-get -y install make || if [ ${?} -gt 0 ]; then exit 1; fi
